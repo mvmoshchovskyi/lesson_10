c203db45a9ef1e2ec2152c39ebbea5448b3309b2
mvmoshchovskyi <mvmoshchovskyi@gmail.com>
2020-04-25 18:39:53 +0300

    2

--- main.js
+++ main.js
@@ -2,39 +2,38 @@ function userCard(number) {
 
     const date = new Date()
 
-    const CardOptions = {
+    const card = {
+        key: 2,
         balance: 100,
         transactionLimit: 100,
         historyLogs: [
-            {operationType:'', credits: '', operationTime: `${date}`},
-            // {operationType: '', credits: '', operationTime: `${date}`},
-            // {operationType: '', credits: '', operationTime: `${date}`},
+            {operationType: '', credits: '', operationTime: `${date}`},
         ],
-        key:number
     }
     return {
         getCardOptions: function () {
-            return CardOptions
-            //(`balance: ${CardOptions.balance},`)
+            // let CardOptions = (        {key:2, balance: 100, transactionLimit: 100})
+            // return cardOptions
+            // {operationType:`${receivedCredits}`,credits: `${newCredit}` , operationTime: `${date}`}
         },
         putCredits: function (money) {
-            let receivedCredits = (CardOptions.balance + money)
+            let receivedCredits = (card.balance + money)
             return receivedCredits
         },
         takeCredits: function (money) {
-            if (money > CardOptions.balance && transactionLimit > 0) {
+            if (money > card.balance && transactionLimit > 0) {
                 console.error('недостатньо коштів')
             } else {
-                let widhdrawalOfCredits = (CardOptions.balance - money)
+                let widhdrawalOfCredits = (card.balance - money)
                 return widhdrawalOfCredits
             }
         },
         setTransactionLimit: function (nLimit) {
-            transactionLimit = CardOptions.transactionLimit = nLimit
+            transactionLimit = card.transactionLimit = nLimit
         },
         transferCredits: function (credit, card1) {
 
-            if (credit > CardOptions.balance && transactionLimit > 0) {
+            if (credit > card.balance && transactionLimit > 0) {
                 console.error('недостатньо коштів')
             } else {
                 let newCredit = (credit - (credit * 0.005))
@@ -50,3 +49,48 @@ card3.putCredits(120);
 card3.takeCredits(80);
 card3.setTransactionLimit(10);
 card3.transferCredits(50, userCard());
+
+// треба зробити функцію яка буде дивитись та відслідковувати усе що відбувалось
+// зробилась функція якась, викликається і запис
+
+
+// завдання 2
+
+
+class UserAccount {
+    constructor(options) {
+        this.name = options.name
+        this.cards = options.cards
+    }
+
+    addCard() {
+        userCard()
+        card <= 3
+    }
+
+    getCardByKey() {
+return {
+key: 1,
+balance: 150,
+    transactionLimit: 100,
+historyLogs:[{}]
+}
+    }
+}
+
+const user = new UserAccount({
+    name: 'Bob',
+    cards: 3,
+    addCard: true
+});
+
+// let user = new UserAccount('Bob');
+// user.addCard()
+// user.addCard()
+// let card1 = user.getCardByKey(1);
+// let card2 = user.getCardByKey(2);
+// card1.putCredits(500);
+// card1.setTransactionLimit(800);
+// card1.transferCredits(300, card2);
+// card2.takeCredits(50);
+// console.log(card1.getCardOptions());
